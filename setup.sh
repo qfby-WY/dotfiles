fc0b438c068ee699f7ac7fb1c25ad6ffc2a8e0db
qfby-WY <qfbywy@gmail.com>
2025-05-08 23:43:41 +0800

    feat: add update_lazygit function in setup.sh

--- setup.sh
+++ setup.sh
@@ -28,3 +28,23 @@ function install_fzf() {
   git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
   $HOME/.fzf/install
 }
+
+function update_lazygit() {
+  api_url="https://api.github.com/repos/jesseduffield/lazygit/releases/latest"
+  LATEST_VERSION=$(curl -fsS ${api_url} | grep -Po '"tag_name": *"v\K[^"]*')
+  if [[ -x "$(command -v lazygit)" ]]; then
+    CURRENT_VERSION=$(lazygit --version | grep -Po '(?:^|, )version=\K\d+\.\d+\.\d+')
+    if [[ "${CURRENT_VERSION}" == "${LATEST_VERSION}" ]]; then
+      echo "alreadly the latest!"
+      return 0
+    fi
+  fi
+  cd $HOME/Downloads/
+  download_url="https://github.com/jesseduffield/lazygit/releases/download"
+  download_url+="/v${LATEST_VERSION}/lazygit_${LATEST_VERSION}_Linux_x86_64.tar.gz"
+  curl -Lo lazygit.tar.gz ${download_url}
+  tar xf lazygit.tar.gz lazygit
+  install -Dm 755 lazygit $HOME/.local/bin/
+  rm -rf lazygit*
+  cd $HOME
+}
